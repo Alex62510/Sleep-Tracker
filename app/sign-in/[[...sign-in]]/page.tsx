835a0d52d7689d04a6b7da862a6488712c6d50f2
Alex Orlov <a.orlov@pixelartsoft.com>
2025-10-02 11:50:22 +0300

    fix clerk modal

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="ml-20 flex items-center justify-center min-h-screen">
       <SignIn />
     </div>
   );
